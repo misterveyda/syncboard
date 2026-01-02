d32f7d7e2ec636b5ac0b12da174307f5398c6831
misterveyda <veydaszn@gmail.com>
2026-01-02 12:00:34 +0000

    commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// Add the .jsx or .tsx extension specifically if the compiler is complaining
+import App from "./App.tsx"; 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// We use type casting here to be 100% safe for the compiler
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
